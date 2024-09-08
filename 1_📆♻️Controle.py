cf464a0a3a346dc0d411817b10ff0805d292a4b2
lucascarvalhosantiagogmail <lucascarvalhosnt@gmail.com>
2024-09-08 17:09:16 -0300

    Adicionando projeto

---
+++ 1_📆♻️Controle.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import webbrowser
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import plotly.express as px 
+
+
+# CARREGAR OS DADOS DA PLANILHA
+
+if "data" not in st.session_state:
+    all_sheets = pd.read_excel("dataset\Planilha.xlsx", sheet_name=None)
+    df_data = pd.concat(all_sheets.values(), ignore_index=True, join="outer")
+    df_data["Licença-Data de validade"] = pd.to_datetime(df_data["Licença-Data de validade"])
+    st.session_state["data"] = df_data
+    
+else:
+    df_data = st.session_state["data"]
+
+# FUNÇÃO QUE DEFINE A PARA A LOGO
+def imagem(caminho):
+    img = mpimg.imread(caminho)
+    plt.axis("off")
+    plt.imshow(img)
+
+# CONFIGURAÇÃO DA PÁGINA
+st.set_page_config(
+page_title="Plataforma Santiago Engenharia",
+page_icon= imagem(r"C:\Users\User\Desktop\Meioambiente\Python\Santiago.png"),
+layout="wide"
+)
+
+# TÍTULO DA PÁGINA
+st.title("CONTROLE DE LICENÇAS")
+st.header("Empresa: Engenharia LTDA")
+st.logo(r"C:\Users\User\Desktop\Meioambiente\Python\Santiago.png")
+
+# DESCRITIVO INICIAL
+st.subheader("Número de Licenças: 2")
+st.subheader("Código da licença 1: L-1234")
+st.subheader("Código da licença 2: L-5678")
+
+# INSERIR OPÇÃO PARA ESCOLHA DA LICENÇA
+licenca = df_data["Código Licença"].unique()
+licenca_selecionada = st.sidebar.selectbox("Licenças", licenca)
+
+df_filtrado = df_data[df_data["Código Licença"] == licenca_selecionada]
+
+contagem_por_data = df_filtrado.groupby("Licença-Data de validade")["Código Licença"].count().reset_index()
+
+st.divider()
+
+if licenca_selecionada == "L-1234":
+    st.header("Licença L-1234")
+else:
+    st.header("Licença L-5678")
+
+if not df_filtrado.empty:
+    col1, col2, col3, col4, col5 = st.columns(5)
+    col1.metric(label="Data de emissão da licença:", value=df_filtrado["Licença-Data de emissão"].iloc[0].strftime("%d/%m/%Y"))
+    col2.metric(label="Data da validade da licença:", value=df_filtrado["Licença-Data de validade"].iloc[0].strftime("%d/%m/%Y"))
+    col3.metric(label="Dias para vencimento:", value=int(df_filtrado["Dias restantes"].iloc[0]))
+    col4.metric(label="Status:", value=df_filtrado["Status da licença"].iloc[0])
+else:
+    st.warning("Não há licença para a condição selecionada.")
+
+st.divider()
+
+# VENCIMENTO
+
+st.subheader("Vencimento")
+
+# CRIANDO CORES DAS BARRAS DO GRÁFICO
+
+df_filtrado["Cor"] = df_filtrado["Status da licença"].map({
+    "Dentro do prazo": "green",
+    "Vencida": "red",
+    "Renovar": "yellow"
+})
+
+# 1º GRÁFICO
+
+contagem_por_data = contagem_por_data.merge(df_filtrado[["Licença-Data de validade", "Cor"]].drop_duplicates(), on="Licença-Data de validade", how="left") 
+
+
+
+fig = px.bar(
+    contagem_por_data,
+    x="Licença-Data de validade",
+    y="Código Licença",
+    color="Cor",
+    labels={"Licença-Data de validade": "Data de Validade", "Quantidade de licenças": "Número de Licenças"},
+    template="plotly_dark",
+    color_discrete_map={"green":"green","red":"red","yellow":"yellow"}
+)
+
+#CONFIGURANDO O TAMANHO DO GRÁFICO
+fig.update_layout(
+    width=600,
+    height=400
+)
+
+col1, col2, col3 = st.columns([1, 2, 1])
+
+with col2:
+    st.plotly_chart(fig)
+
+st.divider()
+st.sidebar.markdown("Desenvolvido por Santiago Engenharia (https://santiagoengenharia.com)")
---
+++ pages/2_📊📈Condicionantes.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+import webbrowser
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import plotly.express as px 
+import plotly.graph_objects as go
+
+# FUNÇÃO QUE DEFINE A PARA A LOGO
+def imagem(caminho):
+    img = mpimg.imread(caminho)
+    plt.axis("off")
+    plt.imshow(img)
+
+# CONFIGURAÇÃO DA PÁGINA
+st.set_page_config(
+page_title="Plataforma Santiago Engenharia",
+page_icon= imagem(r"C:\Users\User\Desktop\Meioambiente\Python\Santiago.png"),
+layout="wide"
+)
+
+# TÍTULO DA PÁGINA
+st.title("CONDICIONANTES AMBIENTAIS")
+st.header("Empresa: Engenharia LTDA")
+st.logo(r"C:\Users\User\Desktop\Meioambiente\Python\Santiago.png")
+
+
+if "data" in st.session_state:
+    df_data = st.session_state["data"]
+    df_data = df_data.dropna(subset=["Número da condicionante",
+                                     "Condicionantes ambientais",
+                                     "Status das condicionantes",
+                                     "Comentário",
+                                     "Código Licença"])
+    df_data = df_data[(df_data["Número da condicionante"] != "") &
+                      (df_data["Condicionantes ambientais"] != "") &
+                      (df_data["Status das condicionantes"] != "") &
+                      (df_data["Comentário"] != "") &
+                      (df_data["Código Licença"] != "") ]
+    df_data = df_data.set_index("Número da condicionante")
+    df_data = df_data[["Condicionantes ambientais",
+                       "Status das condicionantes",
+                       "Comentário",
+                       "Código Licença"]]
+
+  
+    licencas = list(df_data["Código Licença"].unique())
+    status = list(df_data["Status das condicionantes"].unique())
+
+    licencas_selecionadas = st.sidebar.multiselect("Selecione a licença", licencas, licencas)
+    status_selecionadas = st.sidebar.multiselect("Selecione o status", status, status)
+
+    col1, col2 = st.sidebar.columns(2)
+    status_filtrar = col1.button("Filtrar")
+   
+    if status_filtrar:
+        df_filtrado = df_data[(df_data["Código Licença"].isin(licencas_selecionadas)) & 
+                              df_data["Status das condicionantes"].isin(status_selecionadas)]
+    else:
+        df_filtrado = df_data
+    
+    col1, col2 = st.columns([2,1])
+
+    with col1:
+        st.dataframe(df_filtrado)
+    
+    with col2:
+        status_counts = df_filtrado["Status das condicionantes"].value_counts()
+        color_map = {
+        "Regularizado": "green",
+        "Acompanhamento": "yellow",
+        "Não Regularizado": "red"}
+
+        fig = px.pie(
+            names=status_counts.index,
+            values=status_counts.values,
+            labels={"names": "Status", "values":"Quantidade"},
+            color=status_counts.index,
+             color_discrete_map=color_map,
+            template="plotly_dark",
+            hole=0.3
+
+        )
+
+        
+        fig.update_layout(
+            title={
+                "text": "Status das Condicionantes",
+                "x": 0.5,
+                "xanchor": "center",
+                "yanchor": "top"
+            }
+        )
+        st.plotly_chart(fig)
+
+
+    total_condicionantes = len(df_filtrado)
+    regularizado_count = df_filtrado["Status das condicionantes"].value_counts().get("Regularizado", 0)
+    porcentagem_regularizado = (regularizado_count / total_condicionantes) * 100 if total_condicionantes > 0 else 0
+    st.header(f"Desempenho Ambiental: {porcentagem_regularizado:.0f}%")
+    st.subheader(f"Compreende-se neste cálculo que, {regularizado_count} itens das condicionantes da licença ambiental, do total de {total_condicionantes} itens, estão deferidos")
+     
+        
+
+else:
+    st.write("Dados não correlacionados")
+
+st.divider()
+st.sidebar.markdown("Desenvolvido por Santiago Engenharia (https://santiagoengenharia.com)")
